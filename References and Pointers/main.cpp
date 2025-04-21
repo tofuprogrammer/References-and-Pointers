ee3bb75dc9186bcf24b5a74448825759283a787a
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-04-21 12:49:21 +0100

    Add pointer and explanations

--- References and Pointers/main.cpp
+++ References and Pointers/main.cpp
@@ -3,9 +3,16 @@
 
 int main()
 {
+    // Creating and using references
     std::string text = "abc";
-    std::string &letters = text;
+    std::string &letters = text; // & can be used to obtain the memory address of a variable
+    std::string* pointer = &text; // A pointer stores a memory address as its value
 
+    // Identical output
     std::cout << text << "\n";
     std::cout << letters << "\n";
+
+    std::cout << "\n";
+    // Outputs the memory address
+    std::cout << &letters << "\n";
 }
